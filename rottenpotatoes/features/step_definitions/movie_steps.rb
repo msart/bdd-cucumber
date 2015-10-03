f9f293d6bb9bda89649ce84da08123a38e850e03
msart <pedromarcondes94@hotmail.com>
2015-10-03 11:24:02 -0300

    part 2 done

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,10 +1,12 @@
 # Add a declarative step here for populating the DB with movies.
-
+rows = 0
 Given /the following movies exist/ do |movies_table|
+  rows = 0
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
     Movie.create!(movie)
+    rows += 1
   end
 end
 
@@ -14,6 +16,7 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
+  flunk "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -24,9 +27,19 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.delete!("\"")
+  if uncheck.nil?
+    rating_list.split(',').each do |field|
+      check("ratings["+field.strip+"]")
+    end
+  else
+    rating_list.split(',').each do |field|
+      uncheck("ratings["+field.strip+"]")
+    end
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  flunk "Unimplemented"
+  page.should have_css("table#movies tbody tr",:count => rows.to_i)
 end
